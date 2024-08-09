ecce456b09a4e0aaac891c04fc9bb59a66a24c9f
dudnikgg <dudnik.gg@gmail.com>
2024-08-09 21:40:44 +0300

    fix: adds allow-root for cli

--- scripts/import-db.sh
+++ scripts/import-db.sh
@@ -8,18 +8,18 @@ Green='\033[0;32m' # Green
 source .env
 
 echo "$Green # The project name is: localhost $Color_off"
-echo "$Green # The project live url is: $PROJECT_LIVE_URL $Color_off"
+echo "$Green # The project live url is: $PROJECT_DEV_URL $Color_off"
 
 # Exit if any command fails
 set -e
 
 echo "$Green # Importing via wp cli in docker... $Color_off"
-docker compose run --rm wp-cli wp db import /var/www/db-dump/db_dump.sql
+docker compose run --rm wp-cli wp db import /var/www/db-dump/db_dump.sql --allow-root
 
 echo "$Green # Replacing domain names... $Color_off"
-docker compose run --rm wp-cli wp search-replace $PROJECT_DEV_URL http://localhost --all-tables --url=localhost
+docker compose run --rm wp-cli wp search-replace $PROJECT_DEV_URL http://localhost --all-tables --url=localhost --allow-root
 
-docker compose run --rm wp-cli wp option get siteurl
+docker compose run --rm wp-cli wp option get siteurl --allow-root
 
 echo "$Green # Re-creating local user... $Color_off"
-docker compose run --rm wp-cli wp user create local local@dev.dd --role=administrator --user_pass=local
+docker compose run --rm wp-cli wp user create local local@dev.dd --role=administrator --user_pass=local --allow-root
